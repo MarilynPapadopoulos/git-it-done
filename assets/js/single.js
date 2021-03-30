de52655cc6bceab2654aee801bd55b8a22d23862
MarilynPapadopoulos <marilyn.volk@gmail.com>
2021-03-30 10:31:37 -0400

    worked out possible errors, linked homepage

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,25 @@
 
+var repoNoameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+
+
+var getRepoName = function() {
+    //get repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    
+    if(repoName) {
+        //display repo name on the page
+        repoName.textContent = repoName;
+
+        getRepoIssues(repoName);
+    }
+     else {
+        //if no repo name was given, redirect to homepage 
+        document.location.replace("./index.html");
+    }  
+};
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -9,16 +29,18 @@ var getRepoIssues = function(repo) {
             response.json().then(function(data) {
                 //pass response data to dom function
                 displayIssues(data);
+
+                //check if api has paginated issues
+                if(response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         }
         else {
-            alert("There was a problem with your request!");
+            document.location.replace("./index.html");
         }
     });
-    console.log(repo);
 };
-
-getRepoIssues("facebook/react");
 
 var displayIssues = function(issues){
     if (issues.length === 0) {
@@ -56,3 +78,18 @@ var displayIssues = function(issues){
     }
 
 };
+
+var displayWarning = function(repo) {
+    //add test to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    //append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
+
+
+getRepoName();
